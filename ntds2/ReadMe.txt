3a77fe465a28ae55c0cf3a019131ba05213f05bc
David Maloney <DMaloney@rapid7.com>
2015-03-26 16:05:51 -0500

    Initial commit

---
+++ ntds2/ReadMe.txt
@@ -0,0 +1,40 @@
+========================================================================
+    CONSOLE APPLICATION : ntds2 Project Overview
+========================================================================
+
+AppWizard has created this ntds2 application for you.
+
+This file contains a summary of what you will find in each of the files that
+make up your ntds2 application.
+
+
+ntds2.vcxproj
+    This is the main project file for VC++ projects generated using an Application Wizard.
+    It contains information about the version of Visual C++ that generated the file, and
+    information about the platforms, configurations, and project features selected with the
+    Application Wizard.
+
+ntds2.vcxproj.filters
+    This is the filters file for VC++ projects generated using an Application Wizard. 
+    It contains information about the association between the files in your project 
+    and the filters. This association is used in the IDE to show grouping of files with
+    similar extensions under a specific node (for e.g. ".cpp" files are associated with the
+    "Source Files" filter).
+
+ntds2.cpp
+    This is the main application source file.
+
+/////////////////////////////////////////////////////////////////////////////
+Other standard files:
+
+StdAfx.h, StdAfx.cpp
+    These files are used to build a precompiled header (PCH) file
+    named ntds2.pch and a precompiled types file named StdAfx.obj.
+
+/////////////////////////////////////////////////////////////////////////////
+Other notes:
+
+AppWizard uses "TODO:" comments to indicate parts of the source code you
+should add to or customize.
+
+/////////////////////////////////////////////////////////////////////////////
---
+++ ntds2/ntds2.c
@@ -0,0 +1,112 @@
+// ntds1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <string.h>
+#include <wchar.h>
+#include <process.h>
+#include <Windows.h>
+#include <esent.h>
+#pragma comment(lib, "esent")
+
+typedef struct {
+	TCHAR ntdsPath[255];
+	JET_INSTANCE jetEngine;
+	JET_SESID jetSession;
+	JET_DBID jetDatabase;
+	JET_TABLEID jetTable;
+}jetState;
+
+typedef struct {
+	JET_COLUMNDEF accountName;
+	JET_COLUMNDEF accountSID;
+	JET_COLUMNDEF accountType;
+	JET_COLUMNDEF encryptionKey;
+	JET_COLUMNDEF lmHash;
+	JET_COLUMNDEF lmHistory;
+	JET_COLUMNDEF ntHash;
+	JET_COLUMNDEF ntHistory;
+}ntdsColumns;
+
+JET_ERR engine_startup(jetState *ntdsState){
+	JET_ERR jetError;
+	// Set the Page Size to the highest possibile limit
+	jetError = JetSetSystemParameter(&ntdsState->jetEngine, JET_sesidNil, JET_paramDatabasePageSize, 8192, NULL);
+	if (jetError != JET_errSuccess){
+		return jetError;
+	}
+	// Create our Jet Instance
+	jetError = JetCreateInstance(&ntdsState->jetEngine, "NTDS"); 
+	if (jetError != JET_errSuccess){
+		return jetError;
+	}
+	// Disable crash recovery and transaction logs
+	jetError = JetSetSystemParameter(&ntdsState->jetEngine, JET_sesidNil, JET_paramRecovery, NULL, "Off");
+	if (jetError != JET_errSuccess){
+		return jetError;
+	}
+	// Initialise the Jet instance
+	jetError = JetInit(&ntdsState->jetEngine);
+	if (jetError != JET_errSuccess){
+		return jetError;
+	}
+	return JET_errSuccess;
+}
+
+JET_ERR open_database(jetState *ntdsState){
+	JET_ERR attachStatus = JetAttachDatabase(ntdsState->jetSession, ntdsState->ntdsPath, JET_bitDbReadOnly);
+	if (attachStatus != JET_errSuccess){
+		puts("Unable to attach to the NTDS.dit database!");
+		return attachStatus;
+	}
+	JET_ERR openStatus = JetOpenDatabase(ntdsState->jetSession, ntdsState->ntdsPath, NULL, &ntdsState->jetDatabase, JET_bitDbReadOnly);
+	if (openStatus != JET_errSuccess){
+		puts("Unable to open the NTDS.dit database for reading");
+		return openStatus;
+	}
+	return JET_errSuccess;
+}
+
+int _tmain(int argc, TCHAR* argv[])
+{
+	// Create our state structure to track the various info we need
+	jetState *ntdsState = malloc(sizeof(jetState));
+	memset(ntdsState, 0, sizeof(jetState));
+	// Exit if we weren't given an argument
+	if (argc < 2){
+		puts("A path to the NTDS.dit file was not supplied!");
+		exit(2);
+	}
+	TCHAR commandString[300] = "esentutl /p /o ";
+	lstrcpyn(ntdsState->ntdsPath, argv[1], 255);
+	// Check that the path to the NTDS.dit file actually exists
+	if (0xffffffff == GetFileAttributes(ntdsState->ntdsPath)){
+		puts("Cannot access the specified file!");
+		exit(2);
+	}
+	_tcsncat(commandString, ntdsState->ntdsPath, 255);
+	_putts(commandString);
+	// Call the ESENTUTL utility on our NTDS.dit file
+	_tsystem(commandString);
+	JET_ERR startupStatus = engine_startup(ntdsState);
+	if (startupStatus != JET_errSuccess){
+		puts("Error Starting up the Jet Engine!");
+		exit(startupStatus);
+	}
+
+	// Start a Session in the Jet Instance
+	JET_ERR sessionStatus = JetBeginSession(ntdsState->jetEngine, &ntdsState->jetSession, NULL, NULL);
+	if (sessionStatus != JET_errSuccess){
+		puts("Unable to establish a JET Session!");
+		exit(sessionStatus);
+	}
+	JET_ERR openStatus = open_database(ntdsState);
+	if (openStatus != JET_errSuccess){
+		puts("Unable to work with this database file. Exiting..");
+		exit(openStatus);
+	}
+
+	return 0;
+}
+
